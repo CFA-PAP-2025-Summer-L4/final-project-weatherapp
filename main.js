e2016ba4670cceaa82133e578a7f8adbf91646ce
helen-oo <xhelen0729@gmail.com>
2025-07-29 17:13:59 -0700

    Finished my section

--- main.js
+++ main.js
@@ -1,2 +1,36 @@
-// Open-Meteo API for weather data
-const url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+// Fetch Fun Fact from the API
+const funFactApi = "https://api.api-ninjas.com/v1/facts?";
+const  funFactApiKey = "2PEZcWfNMed2t9E75rD0KA==f0IhxzBGJslzMW7C";
+const fact = document.querySelector("#fun-fact");
+
+// authenticate the API key
+const params = {
+    method: "GET",
+    headers: {
+        "X-Api-Key": funFactApiKey
+    }
+};
+
+async function fetchFunFact() {
+    try {
+        const response = await fetch(funFactApi, params);
+        console.log("Response from fun fact API:", response);
+        return await response.json();
+    } catch (error) {
+        console.error("Error fetching fun fact:", error);
+        fact.textContent = "Could not fetch fun fact. Please try again later.";
+    }
+}
+
+fetchFunFact();
+
+async function showFunFact() {
+    let facts = await fetchFunFact();
+    if (facts && facts.length > 0) {
+        fact.textContent = facts[0].fact;
+    } else {
+        fact.textContent = "No fun fact available at the moment.";
+    }
+}
+
+showFunFact();
