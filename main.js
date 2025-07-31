1ee6f22952433541f2bf29564ecc407b57772bac
helen-oo <xhelen0729@gmail.com>
2025-07-31 14:17:08 -0700

    Finished my part

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const airQuality = document.querySelector("#air-quality");
 const humidity = document.querySelector("#humidity");
 const uvIndex = document.querySelector("#uv-index");
 const wind = document.querySelector("#wind");
+const pressure = document.querySelector("#pressure");
 const fact = document.querySelector("#fun-fact");
 
 
@@ -46,8 +47,8 @@ showFunFact();
 
 // Weather Api
 const weatherApiKey = "e1116162e8bb4d738bc190848253107";
-let zipCode = "98108"; 
-const weatherApi = `https://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${zipCode}&aqi=yes`;
+let city = "Seattle";
+const weatherApi = `https://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${city}&aqi=yes`;
 
 const params2 = {
     method: "GET",
@@ -66,6 +67,10 @@ async function fetchWeather() {
     } catch (error) {
         console.error("Error fetching weather data:", error);
         airQuality.textContent = "Could not fetch air quality data. Please try again later.";
+        uvIndex.textContent = "Could not fetch UV data. Please try again later.";
+        humidity.textContent = "Could not fetch humidity data. Please try again later.";
+        wind.textContent = "Could not fetch wind data. Please try again later.";
+        pressure.textContent = "Could not fetch pressure data. Please try again later.";
     }
 }
 
@@ -73,16 +78,36 @@ async function showWeather() {
     let weatherData = await fetchWeather();
 
     if (weatherData && weatherData.current) {
-        airQuality.textContent = `${weatherData.current.air_quality.no2}`;
+        const epaIndex = weatherData.current.air_quality["us-epa-index"];
+        airQuality.textContent = `${epaIndex} - ${epaStatus(epaIndex)}`;
         uvIndex.textContent = `${weatherData.current.uv}`;
         humidity.textContent = `${weatherData.current.humidity}%`;
-        wind.textContent = `${weatherData.current.wind_mph} mph`;
+        wind.textContent = `${weatherData.current.wind_kph} mph ${weatherData.current.wind_dir}`;
+        pressure.textContent = `${weatherData.current.pressure_in} inHg`;
     } else {
         airQuality.textContent = "No air quality data available.";
         humidity.textContent = "No humidity data available.";
         uvIndex.textContent = "No UV index data available.";
         wind.textContent = "No wind data available.";
+        pressure.textContent = "No pressure data available.";
     }
 }
+
 showWeather();
+
+function epaStatus(epaIndex) {
+    if (epaIndex === 1) {
+        return "Good";
+    } else if (epaIndex === 2) {
+        return "Moderate";
+    } else if (epaIndex === 3 || epaIndex === 4) {
+        return "Unhealthy";
+    } else if (epaIndex === 5) {
+        return "Very Unhealthy";
+    } else if (epaIndex === 6) {
+        return "Hazardous";
+    } else {
+        return "Unknown";
+    }
+}
 
